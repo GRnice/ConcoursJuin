9611e815a707bf0f8594b2d023a17c761be51dd6
GRnice <GRnice@users.noreply.github.com>
2016-05-24 14:34:07 +0200

    Update smartStack.cpp

--- problemes/smartStack/SSA2/cpp/smartStack.cpp
+++ problemes/smartStack/SSA2/cpp/smartStack.cpp
@@ -3,8 +3,12 @@
 #include <iostream>
 #include <array>
 #include <string>
-// SMARTSTACK
+#include <vector>
+
 using namespace std;
+
+//SMARTSTACK
+
 
 int getNumber(string ordre)
 {
@@ -15,6 +19,21 @@ int getNumber(string ordre)
 
 int main()
 {
+	std::string input_line;
+	std::vector<std::string> vect;
+	// echo PUSHBACK\nkl| TER.exe
+	while (cin)
+	{
+		getline(cin, input_line);
+		vect.push_back(input_line);
+		//cout << input_line.size() << endl;
+		//cout << input_line << endl;
+	}
+	for (int i = 0; i < vect.size(); i++)
+	{
+		cout << vect.at(i) << endl;
+	}
+
 	int* stack = (int*)malloc(sizeof(int) * 16);
 	int size = 16;
 	int top = 0;
@@ -25,22 +44,27 @@ int main()
 	myarray[2] = string("BASEPOP");
 	myarray[3] = string("BASEPOP");
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < vect.size() ; i++)
 	{
-		if (myarray[i].at(1) == 'U' && top < 16) // pUsh
+		if (vect.at(i).at(1) == 'U' && top < 16) // pUsh
 		{
 			stack[top] = getNumber(myarray[i]);
 			top++;
 		}
-		else if (myarray[i].at(1) == 'O' && top > base) // pOp
+		else if (vect.at(i).at(1) == 'O' && top > base) // pOp
 		{
 			top--;
 			cout << stack[top] << endl;
 		}
-		else if(base < top)
+		else if(base < top) // BASEPOP
 		{
 			cout << stack[base] << endl;
 			base++;
+		}
+		else
+		{
+			// PRINTSTACK
+
 		}
 	}
 
